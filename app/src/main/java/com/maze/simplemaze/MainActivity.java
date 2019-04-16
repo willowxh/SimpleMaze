06d557981d6956e4bbaa4a01fbe5dad53c308122
Harley <tamariskx@qq.com>
2019-04-16 23:08:49 +0800

    加入背景音乐

--- app/src/main/java/com/maze/simplemaze/MainActivity.java
+++ app/src/main/java/com/maze/simplemaze/MainActivity.java
@@ -1,5 +1,10 @@
 package com.maze.simplemaze;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +12,7 @@ import android.view.WindowManager;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.Toast;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     
@@ -14,6 +20,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ImageView rank;
     ImageView sound;
     ImageView play;
+    MediaPlayer mp = null;
+    Boolean isPlayMusic = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setting.setOnClickListener(this);
         rank.setOnClickListener(this);
         sound.setOnClickListener(this);
+
+        playMusic();
     }
 
     @Override
@@ -38,16 +48,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch(v.getId()){
             case R.id.play:
                 Toast.makeText(this, "tap paly", Toast.LENGTH_SHORT).show();
+                Intent intent_game = new Intent(MainActivity.this,GameActivity.class);
+                startActivity(intent_game);
                 break;
             case R.id.setting:
                 Toast.makeText(this, "tap setting", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.rank:
+                Intent intent_rank = new Intent(MainActivity.this,RankActivity.class);
+                startActivity(intent_rank);
                 Toast.makeText(this, "tap rank", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.sound:
-                Toast.makeText(this, "tap sound", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(this, "tap sound", Toast.LENGTH_SHORT).show();
+                if (isPlayMusic){
+                    if(mp != null)
+                        mp.pause();
+                    sound.setImageResource(R.drawable.sound2);
+                    isPlayMusic = false;
+                }else{
+                    if(mp != null)
+                        mp.start();
+                    sound.setImageResource(R.drawable.sound3);
+                    isPlayMusic = true;
+                }
                 break;
         }
+    }
+
+    @SuppressLint("NewApi")
+    public void playMusic(){
+        AssetManager am = getAssets();
+        AssetFileDescriptor afd = null;
+        if(mp == null) {
+            mp = new MediaPlayer();
+        }
+        try {
+            afd = am.openFd("bg_hdl.mp3");
+            mp.setDataSource(afd);
+            mp.prepare();
+            mp.setLooping(true);
+            mp.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mp != null && mp.isPlaying()) {
+                        mp.stop();
+                         mp.release();
+                        mp = null;
+                  }
+        super.onDestroy();
     }
 }
